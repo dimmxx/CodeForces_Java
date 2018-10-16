b61c268da332e66593c92582d68b10c8ec5d87ff
dimmxx <dimmxx@gmail.com>
2018-10-16 19:00:33 +0300

    16-10-18

---
+++ src/codeforces/temp1.java
@@ -0,0 +1,14 @@
+package codeforces;
+
+public class temp1 {
+
+     public static void main (String[] args){
+
+         int i = 3;
+         i += i;
+
+
+            System.out.print(i);
+        }
+   }
+
