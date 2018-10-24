ad86725aeeddf83cc5d6c71b6ef73973074c40f4
dimmxx <dimmxx@gmail.com>
2018-10-24 17:50:09 +0300

    23.10.18

--- src/codeforces/evenOddsBigInt_318A.java
+++ src/codeforces/evenOddsBigInt_318A.java
@@ -3,23 +3,38 @@ import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.Arrays;
 
-
 public class evenOddsBigInt_318A {
-
 
     public static void main (String[] args) {
 
-        //Scanner scanner = new Scanner(System.in);
-        //String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
         // 1 000 000 000 000, 500 000 000 001
-        String input = "1000000000000 500000000001";
+        //String input = "1000000000000 500000000001";
+        //String input = "7 7";
         String[] line = input.split(" ");
         BigInteger[] lineInt = new BigInteger[line.length];
         for (int i = 0; i < line.length; i++) lineInt[i] = new BigInteger(line[i]);
 
-        System.out.println(Arrays.toString(lineInt));
+        BigInteger MAX = lineInt[0];
+        BigInteger IDX = lineInt[1];
+        BigInteger TWO = new BigInteger("2");
+        boolean isEven = MAX.mod(TWO).equals(BigInteger.ZERO);
+        BigInteger result = null;
 
+        BigInteger temp = MAX.divide(TWO);
 
+        if (isEven){
+            if(IDX.compareTo(MAX.divide(TWO)) == -1 |IDX.compareTo(MAX.divide(TWO)) == 0)
+                result = IDX.multiply(TWO).subtract(BigInteger.ONE);
+            else result = IDX.multiply(TWO).subtract(MAX);
+        }else{
+            if(IDX.compareTo((MAX.add(BigInteger.ONE)).divide(TWO)) == -1
+                                  | IDX.compareTo((MAX.add(BigInteger.ONE)).divide(TWO)) == 0)
+                result = IDX.multiply(TWO).subtract(BigInteger.ONE);
+            else result = (IDX.multiply(TWO).subtract(MAX)).subtract(BigInteger.ONE);
+        }
+        System.out.println(result);
     }
 }
